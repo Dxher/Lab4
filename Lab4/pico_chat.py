bf6fc166851403d556b6f1c5d4d5d8ab46e648ac
Anthony <tonydaher@hotmail.ca>
2025-10-07 23:46:16 -0400

    Revised code

--- Lab4/pico_chat.py
+++ Lab4/pico_chat.py
@@ -1,31 +1,65 @@
 from machine import UART, Pin
-import time
+import uasyncio as asyncio
+import time, sys, uselect
 
-# Set up UART
-uart = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
+# Use UART1 on GP8 (TX) and GP9 (RX)
+uart = UART(1, baudrate=115200, tx=Pin(8), rx=Pin(9))
 
+print("Async UART Chat ready. Type to send messages!")
 
-print("Chat ready. Type a message and press Enter:")
+# Non-blocking standard input setup
+poll = uselect.poll()
+poll.register(sys.stdin, uselect.POLLIN)
 
-# Record the current time that a message has not been sent(in milliseconds)
+# RECEIVE
+# This loop checks for incoming UART data without blocking
+async def uart_receiver():
+    while True:
+        if uart.any():                 # if any data has arrived
+            data = uart.read()         # read all available bytes
+            if data:                   # make sure it's not empty
+                print("[Received]:", data.decode('utf-8').strip()) # decode and print
+        await asyncio.sleep(0.1)       # short pause (lets other tasks run)
+
+# SEND
+# This loop waits for user input and sends it out over UART
+async def user_input_sender():
+    while True:
+        # non-blocking "input": only read if a full line is waiting
+        msg = None
+        if poll.poll(0):                               # don't block
+            msg = sys.stdin.readline().rstrip("\r\n")  # get the line
+
+        if msg:                        # if message not empty
+            uart.write(msg + '\n')     # send message
+            print("[Sent]:", msg)
+            # update last_send so keep-alive timer resets
+            global last_send
+            last_send = time.ticks_ms()
+
+        await asyncio.sleep(0.1)         # yield control to receiver
+
+# KEEP ALIVE
+# This loop sends a heartbeat message every 5 seconds
 last_send = time.ticks_ms()
+async def keep_alive():
+    global last_send
+    while True:
+        if time.ticks_diff(time.ticks_ms(), last_send) > 10000: # 10 seconds elapsed
+            heartbeat_msg = "Heartbeat"
+            uart.write(heartbeat_msg + '\n') # send heartbeat
+            print("[Sent]:", heartbeat_msg)
+            last_send = time.ticks_ms() # reset timer
+        await asyncio.sleep(1)
 
-while True:
-    # RECEIVE
-    if uart.any(): # Check if any data has arrived
-        data = uart.read() # read all available bytes
-        if data: # make sure something was actually read
-            print("Received:", data.decode('utf-8'))
+# MAIN 
+# Run both send and receive tasks at the same time
+async def main():
+    await asyncio.gather(
+        uart_receiver(),
+        user_input_sender(),
+        keep_alive()
+    )
 
-    # SEND
-    msg = input("You: ")
-    if msg: # if the user typed something
-        uart.write(msg + "\n") # send it over UART
-        last_send = time.ticks_ms() # reset the timer
-
-    # KEEP ALIVE
-    if last_send > 10000: # every 10 seconds
-        uart.write("keep alive \n") # send the keep-alive message
-        last_send = time.ticks_ms() # reset timer again
-
-    time.sleep(0.1) # pause briefly, good for cpu
+# Call main
+asyncio.run(main())
